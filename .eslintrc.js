a8790b9cb99f752bfc4f535111f30f5e671b1487
Alvin Tan <masloord@gmail.com>
2023-05-18 17:00:01 +0800

    Saving files before refreshing line endings

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 0,
   },
 };
